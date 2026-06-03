2667386f326680d3d7bc05cd66d9f4b265e9fc8e
Florencia Zoffi <136618657+florzoffi@users.noreply.github.com>
2026-06-03 19:11:20 -0300

    aruco detector

---
+++ tpf/tpf/aruco_detector.py
@@ -0,0 +1,97 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import numpy as np
+import cv2
+
+
+class ArucoDetector(Node):
+    def __init__(self):
+        super().__init__("aruco_detector")
+
+        self.bridge = CvBridge()
+
+        self.image_sub = self.create_subscription(
+            Image,
+            "/tb4_0/oakd/rgb/preview/image_raw",
+            self.image_callback,
+            10
+        )
+
+        self.aruco_dict = cv2.aruco.getPredefinedDictionary(
+            cv2.aruco.DICT_4X4_50
+        )
+
+        self.aruco_params = cv2.aruco.DetectorParameters()
+        self.detector = cv2.aruco.ArucoDetector(
+            self.aruco_dict,
+            self.aruco_params
+        )
+
+        self.get_logger().info("Aruco detector node started")
+        self.marker_size = 0.0889
+
+        self.camera_matrix = np.array([
+            [203.14, 0.0, 122.57],
+            [0.0, 361.13, 123.33],
+            [0.0, 0.0, 1.0]
+        ], dtype=np.float64)
+
+        self.dist_coeffs = np.array([
+            -0.9904393553733826,
+            -47.16939926147461,
+            -0.0007601691759191453,
+            -0.00031758102704770863,
+            306.0343933105469
+        ], dtype=np.float64)
+
+    def image_callback(self, msg):
+        frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        corners, ids, rejected = self.detector.detectMarkers(gray)
+
+        if ids is not None:
+            rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(
+                corners,
+                self.marker_size,
+                self.camera_matrix,
+                self.dist_coeffs
+            )
+            ids_flat = ids.flatten()
+            cv2.aruco.drawDetectedMarkers(frame, corners, ids)
+
+            for i, tag_id in enumerate( ids_flat ):
+                tvec = tvecs[i][0]
+
+                x = tvec[0]
+                y = tvec[1]
+                z = tvec[2]
+
+                distance = np.linalg.norm(tvec)
+
+                bearing = np.arctan2(x, z)
+
+                self.get_logger().info(
+                    f"Tag {tag_id} | dist={distance:.2f}m | bearing={bearing:.2f}rad"
+                )
+
+        cv2.imshow("Aruco detections", frame)
+        cv2.waitKey(1)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = ArucoDetector()
+    rclpy.spin(node)
+
+    node.destroy_node()
+    rclpy.shutdown()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
