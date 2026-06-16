37d0d10339f7d5584e03fdc12bf154bdf2e07a0b
Florencia Zoffi <136618657+florzoffi@users.noreply.github.com>
2026-06-16 13:06:34 -0300

    graphslam update

---
+++ tpf/launch/tp_final.launch.py
@@ -0,0 +1,45 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+from ament_index_python.packages import get_package_share_directory
+import os
+
+
+def generate_launch_description():
+
+    rviz_config = os.path.join(
+        get_package_share_directory("tpf"),
+        "rviz",
+        "tpf.rviz",
+    )
+
+    corrected_map_node = Node(
+        package="tpf",
+        executable="corrected_map_node",
+        name="corrected_map_node",
+        output="screen",
+        parameters=[{"use_sim_time": True}],
+    )
+
+    landmarks_publisher = Node(
+        package="tpf",
+        executable="landmarks_publisher",
+        name="landmarks_publisher",
+        output="screen",
+        parameters=[{"use_sim_time": True}],
+    )
+
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=["-d", rviz_config],
+        parameters=[{"use_sim_time": True}],
+    )
+
+    return LaunchDescription([
+        corrected_map_node,
+        landmarks_publisher,
+        rviz,
+    ])
